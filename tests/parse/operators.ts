1be8365e43d598646ec21438cd94b9a0eccf4b81
kgram <kristoffer.gram@gmail.com>
2017-04-27 01:11:37 +0200

    Add parser operators tests

---
+++ tests/parse/operators.ts
@@ -0,0 +1,54 @@
+import { parse } from '../../src/parser'
+
+import { buildNumber, buildVariable } from '../../src/helpers'
+
+function runTest(operator) {
+    // Two numbers, no space
+    expect(parse(`3${operator}2`)).toEqual({
+        type: 'operator',
+        operator,
+        a: buildNumber(3),
+        b: buildNumber(2),
+    })
+    // Number, variable, spaces
+    expect(parse(`x ${operator} 1000`)).toEqual({
+        type: 'operator',
+        operator,
+        a: buildVariable('x'),
+        b: buildNumber(1000),
+    })
+}
+
+describe('operators', () => {
+    test('plus', () => {
+        runTest('+')
+    })
+
+    test('minus', () => {
+        runTest('-')
+    })
+
+    test('plus-minus', () => {
+        runTest('±')
+    })
+
+    test('multiplication', () => {
+        runTest('*')
+    })
+
+    test('multiplication (implied)', () => {
+        runTest(' ')
+    })
+
+    test('division', () => {
+        runTest('/')
+    })
+
+    test('division (inline)', () => {
+        runTest('÷')
+    })
+
+    test('exponentiation', () => {
+        runTest('^')
+    })
+})
