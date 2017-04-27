9344b72bd52e3ef40b60e36204d418041097610b
kgram <kristoffer.gram@gmail.com>
2017-04-27 22:48:21 +0200

    Renderer: fix fraction stories to new format

--- src/renderer/fraction.stories.tsx
+++ src/renderer/fraction.stories.tsx
@@ -4,42 +4,46 @@ import { storiesOf } from '@kadira/storybook'
 import '../stories.scss'
 import '../style.scss'
 
+import Equation from '../equation'
 import EquationWrapper from '../equation-wrapper.stories'
 
 storiesOf('Fractions', module)
+    .addDecorator((story) => (
+        <EquationWrapper>{story()}</EquationWrapper>
+    ))
     .add('Simple', () => (
-        <EquationWrapper>3/4</EquationWrapper>
+        <Equation evaluate>3/4</Equation>
     ))
     .add('Large numerator', () => (
-        <EquationWrapper>(5+2*7)/4</EquationWrapper>
+        <Equation evaluate>(5+2*7)/4</Equation>
     ))
     .add('Large denominator', () => (
-        <EquationWrapper>2/(533*23+252)</EquationWrapper>
+        <Equation evaluate>2/(533*23+252)</Equation>
     ))
     .add('Negated', () => (
-        <EquationWrapper>-2/3</EquationWrapper>
+        <Equation evaluate>-2/3</Equation>
     ))
     .add('Negated, uneven height', () => (
-        <EquationWrapper>-2/(2^2)</EquationWrapper>
+        <Equation evaluate>-2/(2^2)</Equation>
     ))
     .add('Nested', () => (
-        <EquationWrapper>2/(10*2/(533*23+252))</EquationWrapper>
+        <Equation evaluate>2/(10*2/(533*23+252))</Equation>
     ))
     .add('Nested, complex', () => (
-        <EquationWrapper>3/4*(2+2/533*23)/(23+252)*2</EquationWrapper>
+        <Equation evaluate>3/4*(2+2/533*23)/(23+252)*2</Equation>
     ))
     .add('Tall above', () => (
-        <EquationWrapper>1/2/3/4/5/6/7/8/9/10</EquationWrapper>
+        <Equation evaluate>1/2/3/4/5/6/7/8/9/10</Equation>
     ))
     .add('Tall below', () => (
-        <EquationWrapper>1/(2/(3/(4/(5/(6/(7/(8/(9/10))))))))</EquationWrapper>
+        <Equation evaluate>1/(2/(3/(4/(5/(6/(7/(8/(9/10))))))))</Equation>
     ))
     .add('Tall above /w more', () => (
-        <EquationWrapper>(2*(2*(2*(2*(2*(2*(2*(2*1/2)/3)/4)/5)/6)/7)/8)/9)/10</EquationWrapper>
+        <Equation evaluate>(2*(2*(2*(2*(2*(2*(2*(2*1/2)/3)/4)/5)/6)/7)/8)/9)/10</Equation>
     ))
     .add('Tall below /w more', () => (
-        <EquationWrapper>1/(2*2/(2*3/(2*4/(2*5/(2*6/(2*7/(2*8/(2*9/10))))))))</EquationWrapper>
+        <Equation evaluate>1/(2*2/(2*3/(2*4/(2*5/(2*6/(2*7/(2*8/(2*9/10))))))))</Equation>
     ))
     .add('Uneven heights', () => (
-        <EquationWrapper>1/(2/3) * (1/2)/3</EquationWrapper>
+        <Equation evaluate>1/(2/3) * (1/2)/3</Equation>
     ))
