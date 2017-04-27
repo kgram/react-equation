a39fe4a44ac62fc70a4799e993543447187ed57c
kgram <kristoffer.gram@gmail.com>
2017-04-27 23:28:34 +0200

    Parser REPL: add text info + 3x size

--- src/parser/stories.tsx
+++ src/parser/stories.tsx
@@ -25,7 +25,7 @@ function Code({children}: {children?: JSX.Element}) {
     return <div><pre className={classes.equationWrapperRaw}>{children}</pre></div>
 }
 
-function Math({children = []}: {children?: string[]}) {
+function Math({children = [], largeSize}: {children?: string[], largeSize: boolean}) {
     const variableLookup: VariableLookup = {}
     const functionLookup: FunctionLookup = {}
     const equations = children.map((input) => {
@@ -76,7 +76,7 @@ function Math({children = []}: {children?: string[]}) {
 
     return (
         <div>
-            <div>
+            <div style={{ fontSize: largeSize ? '300%' : '100%' }}>
                 {equations.map(({ input, variables, functions }, idx) => (
                     <div key={idx}>
                         <div className={classes.equationWrapper}>
@@ -105,13 +105,16 @@ function Math({children = []}: {children?: string[]}) {
     )
 }
 
-class Editor extends React.Component<null, {value: string}> {
+class Editor extends React.Component<null, {value: string, largeSize: boolean}> {
     constructor(props: null) {
         super(props)
         this.state = {
             value: getPersistantState(),
+            largeSize: false,
         }
     }
+
+    handleSizeChange = (e: React.FormEvent<HTMLInputElement>) => this.setState({largeSize: e.currentTarget.checked})
 
     handleChange = (e: React.FormEvent<HTMLTextAreaElement>) => {
         const value = e.currentTarget.value
@@ -123,14 +126,31 @@ class Editor extends React.Component<null, {value: string}> {
         const equations = this.state.value.split(/\n/g).map((s) => s.trim()).filter((s) => s)
         return (
             <div>
-                <div>
+                <div className={classes.sizeControl}>
+                    <label>
+                        <input
+                            type='checkbox'
+                            checked={this.state.largeSize}
+                            onChange={this.handleSizeChange}
+                        />
+                        3x font
+                    </label>
+                </div>
+                <div style={{display: 'flex', flexDirection: 'row'}}>
                     <textarea
                         className={classes.equationWrapperRaw}
                         onChange={this.handleChange}
                         value={this.state.value}
                     />
+                    <div style={{ maxWidth: 400 }}>
+                        <p>Write equations in plain-text split into lines. Content is kept in localstorage.</p>
+                        <p>Below, the equation tree and stringified equation are shown.</p>
+                        <p>Multiplication without symbol is only supported with a space ("2 x", not "2x"). Hopefully this can be resolved.</p>
+                        <p>Common variables and units are available.</p>
+                        <p>Variables and functions can be assigned and used later.</p>
+                    </div>
                 </div>
-                <Math>{equations}</Math>
+                <Math largeSize={this.state.largeSize}>{equations}</Math>
             </div>
         )
     }
