ffacea4c4b6dc802b54ab3ac7f7d884ee8d3c61d
Kristoffer Gram <kristoffer.gram@gmail.com>
2017-07-23 18:13:08 +0200

    Jest nearley-preprocessor: run babel-preprocessor on result

--- jest-nearley-preprocessor.js
+++ jest-nearley-preprocessor.js
@@ -3,12 +3,20 @@ var compile = require('nearley/lib/compile')
 var generate = require('nearley/lib/generate')
 var { ParserRules, ParserStart } = require('nearley/lib/nearley-language-bootstrapped')
 
-module.exports = {
-    process: function (src) {
-        var parser = new Parser(ParserRules, ParserStart)
-        parser.feed(src)
+var babelPreprocessor = require('babel-jest')
 
-        var compilation = compile(parser.results[0], {})
-        return generate(compilation, 'Grammar').replace('module.exports ?= ?', 'module.exports={};module.exports.default=')
+function parseNearley(src) {
+    var parser = new Parser(ParserRules, ParserStart)
+    parser.feed(src)
+
+    var compilation = compile(parser.results[0], {})
+    return generate(compilation, 'Grammar').replace('module.exports ?= ?', 'module.exports={};module.exports.default=')
+}
+
+module.exports = {
+    process: function (src, filename, config) {
+        const jsSrc = parseNearley(src)
+
+        return babelPreprocessor.process(jsSrc, filename.replace(/\.ne$/, '.js', config))
     }
 }
